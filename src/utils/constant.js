29feea36738aceee70f9ca9089d2d18def133a0d
ShouDuo <handsomesaywhynot@gmail.com>
2023-11-19 00:00:33 +0800

    feat: :art: change default color set

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,39 +1,8 @@
 export const DEFAULT_COLORS = [
-  {
-    id: 0,
-    key: 'head',
-    name: '头部',
-    color: "#59c09a"
-  },
-  {
-    id: 1,
-    key: 'body',
-    name: '身体',
-    color: "#02a004"
-  },
-  {
-    id: 2,
-    key: 'arm',
-    name: '手臂',
-    color: "#a9c09a"
-  },
-  {
-    id: 3,
-    key: 'hand',
-    name: '手掌',
-    color: "#59d09a"
-  },
-  {
-    id: 4,
-    key: 'leg',
-    name: '腿部',
-    color: "#19c09c"
-  },
-  {
-    id: 5,
-    key: 'shoe',
-    name: '鞋子',
-    color: "#09ccad"
-  },
-  
-]
+  { id: 0, key: "head", name: "头部", color: "#fdab2f" },
+  { id: 1, key: "body", name: "身体", color: "#8cff23" },
+  { id: 2, key: "arm", name: "手臂", color: "#8cff23" },
+  { id: 3, key: "hand", name: "手掌", color: "#fdab2f" },
+  { id: 4, key: "leg", name: "腿部", color: "#8cff23" },
+  { id: 5, key: "shoe", name: "鞋子", color: "#8f3ad4" },
+];
